dbc38aab69edc78df73d885cdd66654ee1221c60
Jose_M <102838702+Jose2918@users.noreply.github.com>
2023-01-24 20:55:10 -0600

    Add files via upload

---
+++ Math.js
@@ -0,0 +1,72 @@
+let array = "";
+let letArray = array.length;
+let stringvalue="";
+
+function one(){
+    array = document.getElementById("screen").value = array.concat(1);
+}
+
+function two(){
+    array = document.getElementById("screen").value = array.concat(2);
+}
+
+function three(){
+    array = document.getElementById("screen").value = array.concat(3);
+}
+
+function four(){
+    array = document.getElementById("screen").value = array.concat(4);
+}
+
+function five(){
+    array = document.getElementById("screen").value = array.concat(5);
+}
+
+function six(){
+    array = document.getElementById("screen").value = array.concat(6);
+}
+
+function seven(){
+   array = document.getElementById("screen").value = array.concat(7);
+}
+
+function eigth(){
+    array = document.getElementById("screen").value = array.concat(8);
+}
+
+function nine(){
+    array = document.getElementById("screen").value = array.concat(9);
+}
+
+function cero(){
+    array = document.getElementById("screen").value = array.concat(0);
+}
+
+function div(){
+    array = document.getElementById("screen").value = array.concat("/")
+}
+
+function mul(){
+    array = document.getElementById("screen").value = array.concat("*");
+}
+
+function sub(){
+    array = document.getElementById("screen").value = array.concat("-");
+}
+
+function add(){
+    array = document.getElementById("screen").value = array.concat("+");
+}
+
+function clean(){
+    document.getElementById("screen").value = 0,array="",stringvalue="";
+}
+
+function same(){
+    let result = eval(array);
+    document.getElementById("screen").value = result;
+}
+
+function delet(){
+    document.getElementById("screen").value = array;
+}
